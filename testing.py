fbd77c2eb70debff1bd4b12a9aeb929f12120f1f
alexander <akcremer11@gmail.com>
2025-05-14 00:49:20 +0200

    added code for plotting all

---
+++ testing.py
@@ -0,0 +1,19 @@
+import grpo
+import ppo
+import acrobot_ppo
+import acrobot_grpo
+
+
+modules = [grpo, ppo, acrobot_ppo, acrobot_grpo]
+
+# Loop through each module and call its train function
+for module in modules:
+    if module in [grpo, acrobot_grpo]:
+        for group_size in [2, 4, 10]:
+            for episode in range(1,11):
+                print(f"Episode {episode}: Running train({group_size}) from {module.__name__}")
+                module.train(group_size, seed=episode)
+    else:
+        for episode in range(1,11):
+            print(f"Episode {episode}: Running train({group_size}) from {module.__name__}")
+            module.train(seed=episode)
